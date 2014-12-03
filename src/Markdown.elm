739c7e4de68aa15e194425aeb49ec6a91983baa3
Evan Czaplicki <info@elm-lang.org>
2014-12-02 18:36:10 -0800

    Add parser options

--- src/Markdown.elm
+++ src/Markdown.elm
@@ -4,6 +4,9 @@ module Markdown where
 
 # Parsing Markdown
 @docs toElement, toHtml
+
+# Parsing with Custom Options
+@docs Options, defaultOptions, toElementWith, toHtmlWith
 -}
 
 import Graphics.Element (Element)
@@ -12,7 +15,7 @@ import Native.Markdown
 import Text
 
 
-{-| Turn a markdown string into an HTML element.
+{-| Turn a markdown string into an HTML element, using the `defaultOptions`.
 
     bodyParagraph : Html
     bodyParagraph =
@@ -25,12 +28,11 @@ import Text
     """
 -}
 toHtml : String -> Html
-toHtml =
-    Native.Markdown.toHtml
+toHtml string =
+    Native.Markdown.toHtmlWith defaultOptions string
 
 
-{-| Turn a markdown string into an HTML element.
-
+{-| Turn a markdown string into an HTML element, using the `defaultOptions`.
 
     intro : Element    
     intro =
@@ -43,5 +45,83 @@ toHtml =
     """
 -}
 toElement : String -> Element
-toElement =
-    Native.Markdown.toElement
+toElement string =
+    Native.Markdown.toElementWith defaultOptions string
+
+
+{-| Some parser options so you can tweak things for your particular case.
+
+    * `githubFlavored` &mdash; overall reasonable improvements on the original
+      markdown parser as described [here][gfm]. This includes stuff like [fenced
+      code blocks][fenced]. There are some odd parts though, such as [tables][]
+      and a setting to turn all newlines into newlines in the resulting output,
+      so there are settings to turn those on or off based on your preference.
+
+    * `sanitize` &mdash; this determines if all HTML should be escaped. If you
+      are parsing user markdown or user input can somehow reach the markdown
+      parser, you should almost certainly turn on sanitation. If it is just you
+      writing markdown, turning sanitation off is a nice way to do some HTML
+      tricks if it is needed.
+
+    * `smartypants` &mdash; This will automatically upgrade quotes to the
+      prettier versions and turn dashes into [em dashes or en dashes][dash]
+
+
+[gfm]: https://help.github.com/articles/github-flavored-markdown/
+[fenced]: https://help.github.com/articles/github-flavored-markdown/#fenced-code-blocks
+[tables]: https://help.github.com/articles/github-flavored-markdown/#tables
+[dash]: http://en.wikipedia.org/wiki/Dash
+-}
+type alias Options =
+    { githubFlavored : Maybe { tables : Bool, breaks : Bool }
+    , sanitize : Bool
+    , smartypants : Bool
+    }
+
+
+{-| The `Options` used by the `toElement` and `toHtml` functions.
+
+    { githubFlavored = Just { tables = False, breaks = False }
+    , sanitize = False
+    , smartypants = False
+    }
+-}
+defaultOptions : Options
+defaultOptions =
+    { githubFlavored = Just { tables = False, breaks = False }
+    , sanitize = False
+    , smartypants = False
+    }
+
+
+{-| Maybe you want to parse user input into markdown. To stop them from adding
+`<script>` tags, you can use modified parsing options.
+
+    options : Options
+    options =
+        { defaultOptions | sanitize <- True }
+
+    toMarkdown : String -> Html
+    toMarkdown userInput =
+        Markdown.toHtmlWith options userInput
+-}
+toHtmlWith : Options -> String -> Html
+toHtmlWith =
+    Native.Markdown.toHtmlWith
+
+
+{-| Maybe you want to get prettier quotes with a simple syntax. You can use
+modified parsing options.
+
+    options : Options
+    options =
+        { defaultOptions | smartypants <- True }
+
+    toSmartElement : String -> Element
+    toSmartElement markdown =
+        Markdown.toElementWith options markdown
+-}
+toElementWith : Options -> String -> Element
+toElementWith =
+    Native.Markdown.toElementWith
+
