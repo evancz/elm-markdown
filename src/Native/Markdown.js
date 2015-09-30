2e6895487fe9fc400bdcb03307d0daf7017d1cc4
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2015-09-30 11:16:12 +0200

    fixing type errors in the native stuff

--- src/Native/Markdown.js
+++ src/Native/Markdown.js
@@ -40,8 +40,8 @@ Elm.Native.Markdown.make = function(localRuntime) {
 		{
 			return {
 				gfm: true,
-				tables: gfm.tables,
-				breaks: gfm.breaks,
+				tables: gfm._0.tables,
+				breaks: gfm._0.breaks,
 				sanitize: options.sanitize,
 				smartypants: options.smartypants
 			};
